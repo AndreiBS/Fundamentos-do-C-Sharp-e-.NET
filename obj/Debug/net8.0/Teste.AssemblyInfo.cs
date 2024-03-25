77481d407aaad3ae4eb1f235cb4da231b17e4569
Andrei Batista <andreibds21@gmail.com>
2024-03-25 00:02:40 -0300

    Nova mudanca de pastas

--- obj/Debug/net8.0/Teste.AssemblyInfo.cs
+++ obj/Debug/net8.0/Teste.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Teste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c3d4848d442c9789c88e83b278c9627320a405")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07af999536ec6d193cc69a1ab606d96786dda53d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Teste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Teste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
